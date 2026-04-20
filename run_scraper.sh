a0dbbdc997bdc4f5acc3c8a12e0cb0e675813e7a
andygoodger-svg <andy.goodger@gmail.com>
2026-04-20 09:47:12 +0100

    Auto-update: 2026-04-20 09:47

--- run_scraper.sh
+++ run_scraper.sh
@@ -1,3 +1,2 @@
-#!/bin/sh
-source /Volumes/SSD_1/blaby-venv/bin/activate
-python3 /Volumes/SSD_1/blaby-bowls/scraper_mac.py >> /Volumes/SSD_1/blaby-bowls/scraper.log 2>&1
+#!/bin/bash
+/Volumes/SSD_1/blaby-venv/bin/python3 /Volumes/SSD_1/blaby-bowls/scraper_mac.py >> /Volumes/SSD_1/blaby-bowls/scraper.log 2>&1
